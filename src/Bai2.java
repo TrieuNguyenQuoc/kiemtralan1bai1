3e7297ded1c0b7a0cadcc276a546a2ca38c5d53f
trieunhuyenquoc <trieunguyenquoc020604@gmai.com>
2023-01-11 15:28:39 +0700

    https://github.com/TrieuNguyenQuoc/kiemtralan1bai1

---
+++ src/Bai2.java
@@ -0,0 +1,17 @@
+public class Bai2 {
+        public double maSanPham;
+        public double tenSanPham;
+        public double giaSanpham;
+        public double soLuongSanPham;
+        public double khuyenMai;
+
+        public Bai2(double maSanPham,double tenSanPham, double giaSanpham, double soLuongSanPham, double khuyenMai){
+            this.maSanPham = maSanPham;
+            this.tenSanPham = tenSanPham;
+            this.giaSanpham = giaSanpham;
+            this.soLuongSanPham = soLuongSanPham;
+            this.khuyenMai = khuyenMai;
+
+        }
+
+    }
